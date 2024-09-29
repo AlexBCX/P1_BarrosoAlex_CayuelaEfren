ee36668f3e0fb9a69afc3e9c7ed65f5903a62bda
ecayuelav <ecayuelav@edu.tecnocampus.cat>
2024-09-29 19:48:50 +0200

    Segunda update

--- src/Jugador.java
+++ src/Jugador.java
@@ -18,10 +18,10 @@ public class Jugador <E extends Itipopieza> {
     public boolean eliminarPiezaPosicion(int fil, int col) throws FiJocException {
         E pieza = buscarEnPosicion(fil, col);
         if (pieza != null) {
+            piezasVivas.remove(pieza);
             if(pieza.fiJoc()){
                 throw new FiJocException("Has matat al rei!");
             }
-            piezasVivas.remove(pieza);
             return true;
         }
         return false;
@@ -29,18 +29,29 @@ public class Jugador <E extends Itipopieza> {
 
     public void moverPieza(int fil1, int col1, int fil2, int col2) throws RuntimeException {
         E pieza = buscarEnPosicion(fil1, col1);
-        if (pieza != null) {
-            E pieza2=buscarEnPosicion(fil2,col2);
+        if(pieza == null){
+            throw new RuntimeException("Peça no trobada!");
+        }
 
-            try{
-                pieza.setPosicion(fil2, col2);
-                if(pieza)
-            }
-            catch(RuntimeException e){
-                System.out.println("No pots moure la peça:" + e.getMessage());
+        try{
+            E pieza2 = buscarEnPosicion(fil2, col2);
+
+            pieza.setPosicion(fil2, col2);
+
+            if(pieza2 != null && eliminarPiezaPosicion(fil2,col2)) {
+                System.out.println("Peça matada!");
             }
         }
-        throw new RuntimeException("Peça no trobada!");
+        catch(RuntimeException e){
+            System.out.println("No pots moure la peça:" + e.getMessage());
+        }
+        catch(FiJocException e){
+            System.out.println("Has guanyat: " + e.getMessage());
+        }
+
     }
 
+    public ArrayList<E> getPiezasVivas() {
+        return piezasVivas;
+    }
 }
