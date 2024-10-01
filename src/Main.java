7c0e398de6bbbdc79904d9d375881e864f3c2d4d
AlexBCX <alexxbc1998@gmail.com>
2024-10-01 03:43:49 +0200

    Main comenzador para organizar la estrucitura

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import static java.lang.System.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        out.print("Hello and welcome!");
+    Scanner sc = new Scanner(System.in); //per poder escriure i llegir desde teclat
+    Torns<String> torns=new Torns<>();
+    boolean jugant=true;
 
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        for (int i = 1; i <= 5; i++) System.out.println("i = " + i);
+    String nomFitxer=""; //nom on guardarem les partides
+
+    int opcio;
+
+    while(jugant){
+        System.out.println("Benvingut als escacs!");
+        System.out.println("1. Jugar una partida nova");
+        System.out.println("2. Reproduir una partida des d'un fitxer");
+        System.out.println("3. Sortir");
+        System.out.print("Selecciona una opció: ");
+        opcio = sc.nextInt();
+
+        switch(opcio){
+        case 1:
+            System.out.println("Ingresa el nom del fitxer sense .txt.");
+                nomFitxer=sc.next();
+                jugarPartidaNova(torns);
+            break;
+        case 2:
+            //case de reproduir desde fitxer
+            break;
+        case 3:
+            jugant = false;
+            System.out.println("Sortint del joc...");
+            break;
+        default:
+            System.out.println("Opció no vàlida. Torna-ho a intentar.");
+        }
+    }   //FI WHILE (PROGRAMA)
+    }
+    public static void jugarPartidaNova(Torns<String> torns){
+
+       //inicialitzem les peces i jugadors
+        ArrayList<Pieza>Blanques=new ArrayList<>();
+        ArrayList<Pieza>Negres=new ArrayList<>();
+
+        Jugador<Pieza>JugadorBlanc=new Jugador<>(Blanques);
+        Jugador<Pieza>JugadorNegre=new Jugador<>(Negres);
     }
 }
